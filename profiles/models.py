f33b01d0dd33a60cfc8c16c3a0c41da1cd2552bf
aslavinska <anastasija.slavinska@avl.com>
2023-09-22 08:32:32 +0000

    cleaning base.html

--- profiles/models.py
+++ profiles/models.py
@@ -19,7 +19,6 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True, blank=True)
-    stripe_customer_id = models.CharField(max_length=50)
 
     def __str__(self):
         return self.user.username
