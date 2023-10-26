22c8fa4173e101014df41964e6022cc593affc69
aslavinska <anastasija.slavinska@avl.com>
2023-10-26 13:47:49 +0000

    deployment commit testing

--- bag/contexts.py
+++ bag/contexts.py
@@ -24,7 +24,7 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                total += product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
